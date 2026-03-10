ce976e97ed17e079692b6315d3d9b9750c8ebcfe
Dmitriy Belyaev <6915328+dbelyaev@users.noreply.github.com>
2026-03-10 18:29:34 +0100

    feat(tests): add unit tests for Catmull-Rom spline and ensureMinDist functions

--- rsUtility/catmullRom.h
+++ rsUtility/catmullRom.h
@@ -1,7 +1,21 @@
 /*
  * Copyright (C) 2026  Really Slick Screensavers Contributors
  *
- * Catmull-Rom spline interpolation and distance enforcement utilities.
+ * This file is part of rsUtility.
+ *
+ * rsUtility is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * rsUtility is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 #ifndef CATMULLROM_H
